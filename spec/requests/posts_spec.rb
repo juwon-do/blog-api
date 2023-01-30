1035c0a5509659a6b9ce92191143a5dd3657ac4e
Juwon Do <nobeldr13@gmail.com>
2023-01-30 13:09:04 -0600

    rspec show action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -5,7 +5,48 @@ RSpec.describe "Posts", type: :request do
     it "works! (now write some real specs)" do
       get '/posts'
       expect(response).to have_http_status(200)
+    end
+
+    it "should return all posts in the db" do
+      user = User.create!(
+        email: "test@test.com",
+        password: "password"
+      )
       
+      Post.create!(
+        title: "Hello",
+        body: "This is a new post",
+        image: "google.com",
+        user_id: user.id
+
+      )
+      get "/posts.json"
+      posts = JSON.parse(response.body)      
+      expect(response).to have_http_status(200)
+      expect(posts.length).to eq(1)
+    end
+  end
+
+  describe "GET /posts/:id" do
+    it "should return a post in the db" do
+      user = User.create!(
+        email: "test@test.com",
+        password: "password"
+      )
+      
+      post = Post.create!(
+        title: "Hello",
+        body: "This is a new post",
+        image: "google.com",
+        user_id: user.id
+
+      )
+
+      get "/posts/#{post.id}.json"
+      post = JSON.parse(response.body)
+      expect(post['title']).to eq("Hello")
+      expect(post['body']).to eq("This is a new post")
+      expect(post['image']).to eq("google.com")
     end
   end
 end
